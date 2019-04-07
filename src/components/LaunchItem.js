589e3451f99e3afff2bcec9b085919d414d2d295
Andrew <betelgeuse.as@gmail.com>
2019-04-07 20:43:15 +0300

    Improve view Launch Item

--- src/components/LaunchItem.js
+++ src/components/LaunchItem.js
@@ -2,16 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Row, Col, Card, Button } from 'react-bootstrap';
+import classNames from 'classnames';
+import Moment from 'react-moment';
 
 export const LaunchItem = ({ launch: {flight_number, mission_name, launch_date_local, launch_success} }) => {
   return (
     <>
-      <Card bg="light" border="dark" text="black" className="m-t-10">
+      <Card bg="light" border="dark" text="black" className="mt-3">
         <Card.Body>
           <Row>
             <Col md={9} className="text-left">
-              <Card.Title>Mission: {mission_name}</Card.Title>
-              <Card.Text>Date: {launch_date_local}</Card.Text>
+              <Card.Title>Mission: <span className={classNames({
+                'text-success': launch_success,
+                'text-danger': !launch_success
+              })}>{mission_name}</span></Card.Title>
+              <Card.Text>Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment></Card.Text>
             </Col>
             <Col md={3}>
               <Button variant="secondary">Launch Details</Button>
